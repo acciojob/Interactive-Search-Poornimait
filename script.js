2ca45f3a0794370bac3f5a8d53c64805559f3072
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-04 17:44:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+    function activateSearch() {
+        var searchComponent = document.getElementById("searchComponent");
+
+        // Add the "active" class to the search component
+        searchComponent.classList.add("active");
+
+        // Focus on the input field
+        searchComponent.querySelector(".input").focus();
+    }
